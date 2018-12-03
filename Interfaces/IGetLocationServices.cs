fc85c388307c2587b4d6fbaabd8a69b51ef55051
Андрей Демидюк <andrew.demidyuk@outlook.com>
2018-12-03 10:26:42 +0200

    Add Interface IGetCurrentLocation

---
+++ Interfaces/IGetLocationServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Models;
+
+namespace WeatherApp.Interfaces
+{
+    public interface IGetLocationServices
+    {
+        Task<Location> GetCurrentLocation();
+    }
+}
