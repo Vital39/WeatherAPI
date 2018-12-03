63e60a550dfbbfc2069002032d9636df7da90927
Student <student@lan.itstep.kiev.ua>
2018-12-03 11:08:05 +0200

    Create BusinessWeatherCondition model

---
+++ BusinessModels/BusinessWeatherCondition.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.BusinessModels
+{
+    public class BusinessWeatherCondition
+    {
+        [JsonProperty("latitude")]
+        public double Latitude { get; set; }
+
+        [JsonProperty("longitude")]
+        public double Longitude { get; set; }
+
+        [JsonProperty("timezone")]
+        public string Timezone { get; set; }
+
+        [JsonProperty("currently")]
+        public CurrentForecast CurrentForecast { get; set; }
+
+        [JsonProperty("daily")]
+        public DailyForecast DailyForecast { get; set; }
+    }
+}
