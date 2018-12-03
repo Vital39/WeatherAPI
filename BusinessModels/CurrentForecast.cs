fcf721477eb24dcd28f59637d7b42d793b5c7a39
Student <student@lan.itstep.kiev.ua>
2018-12-03 11:06:34 +0200

    Create CurrentWeatherForecast model

---
+++ BusinessModels/CurrentForecast.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.BusinessModels
+{
+    public class CurrentForecast
+    {
+        [JsonProperty("time")]
+        public int Time { get; set; }
+
+        [JsonProperty("icon")]
+        public string Icon { get; set; }
+
+        [JsonProperty("summary")]
+        public string Summary { get; set; }
+
+        [JsonProperty("temperature")]
+        public double Temperature { get; set; }
+
+        [JsonProperty("windSpeed")]
+        public double WindSpeed { get; set; }
+    }
+    }
+
