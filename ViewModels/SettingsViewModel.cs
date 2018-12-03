53ed148251351bed26e19423841db13b55b73d4d
VitaliyMartinyk <vital39@ukr.net>
2018-12-03 11:31:25 +0200

    Dependency Injection IGeocodingServices

--- ViewModels/SettingsViewModel.cs
+++ ViewModels/SettingsViewModel.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WeatherApp.Interfaces;
 
 namespace WeatherApp.ViewModels
 {
     public class SettingsViewModel
     {
+        private IGeocodingServices geocodingServices;
 
+        public SettingsViewModel(IGeocodingServices geocodingServices)
+        {
+            this.geocodingServices = geocodingServices;
+        }
     }
 }
