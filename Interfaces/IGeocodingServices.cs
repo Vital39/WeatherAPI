41c465e7202449b26bd1a183ab074221b043750e
Anatoliy <Snakeeyes002@gmail.com>
2018-12-03 10:07:28 +0200

    Add Geocoding models

---
+++ Interfaces/IGeocodingServices.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.BusinessModels;
+using WeatherApp.Models.Geocoding;
+
+namespace WeatherApp.Interfaces
+{
+    interface IGeocodingServices
+    {
+        IList<FormattedAddress> GetFormattedAddress(string city_adrress);
+    }
+}
