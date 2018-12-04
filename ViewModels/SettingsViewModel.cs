25e5414b7b84d7353c922dcfd9d682352a8ddf47
VitaliyMartinyk <vital39@ukr.net>
2018-12-04 10:04:46 +0200

    Create addressChangeCommand

--- ViewModels/SettingsViewModel.cs
+++ ViewModels/SettingsViewModel.cs
@@ -4,6 +4,9 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using WeatherApp.BusinessModels;
+using WeatherApp.Commands;
 using WeatherApp.Interfaces;
 
 namespace WeatherApp.ViewModels
@@ -11,6 +14,8 @@ namespace WeatherApp.ViewModels
     public class SettingsViewModel : INotifyPropertyChanged
     {
         private readonly IGeocodingService geocodingService;
+        private SettingsCommand addressChangeCommand;
+        private FormattedAddress selectedAddress;
 
         public SettingsViewModel(IGeocodingService geocodingService)
         {
@@ -18,5 +23,29 @@ namespace WeatherApp.ViewModels
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public ICommand AddressChangeCommand
+        {
+            get
+            {
+                if (addressChangeCommand == null)
+                    addressChangeCommand = new SettingsCommand(GetNewAddresses, CanChangeAddress);
+                return addressChangeCommand;
+            }
+        }
+
+        private bool CanChangeAddress(object parameter)
+        {
+            FormattedAddress currentSelectedAddress = parameter as FormattedAddress;
+            if (selectedAddress == null)
+                return false;
+            return true;
+        }
+
+        private async void GetNewAddresses(object parameter)
+        {
+            await geocodingService.GetFormattedAddressAsync("");
+        }
     }
 }
