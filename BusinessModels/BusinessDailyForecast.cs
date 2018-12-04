54abd43ef0c8f049bd0298b50cea5e2e8137e801
Student <student@lan.itstep.kiev.ua>
2018-12-04 11:39:01 +0200

    Create BusinessDailyForecast model

--- BusinessModels/BusinessDailyForecast.cs
+++ BusinessModels/BusinessDailyForecast.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace WeatherApp.BusinessModels
 {
-    public class BusinessWeatherForecast
+    public class BusinessDailyForecast
     {
+        public string DayWeek { get; set; }
         public string Date { get; set; }
-        public string Time { get; set; }
-        public string Temperature { get; set; }
+        public string RangeTemperature { get; set; }
         public string Icon { get; set; }
         public string WindSpeed { get; set; }
-        public string CloudCover { get; set; }
+
     }
 }
