4accf4f78735aae99eb6a7ab695dedc6e0ba62ed
Student <student@lan.itstep.kiev.ua>
2018-12-04 10:13:06 +0200

    Create BusinessWeatherForecast model

--- BusinessModels/BusinessWeatherForecast.cs
+++ BusinessModels/BusinessWeatherForecast.cs
@@ -8,5 +8,11 @@ namespace WeatherApp.BusinessModels
 {
     class BusinessWeatherForecast
     {
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public string Temperature { get; set; }
+        public string Icon { get; set; }
+        public string WindSpeed { get; set; }
+        public string CloudCover { get; set; }
     }
 }
