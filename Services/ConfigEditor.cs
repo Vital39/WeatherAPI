f5fc7fe1581a46f81dc3ad9803d71bb96b86150b
Андрей Демидюк <andrew.demidyuk@outlook.com>
2018-12-05 10:54:48 +0200

    Add ConfigEdditor

---
+++ Services/ConfigEditor.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Models;
+
+namespace WeatherApp.Services
+{
+    public class ConfigEditor
+    {
+        private const string locationKey = "location";
+        public void SetLoaction(Location location)
+        {
+            ConfigurationManager.AppSettings[locationKey] = JsonConvert.SerializeObject(location);
+        }
+        public Location GetLoaction()
+        {
+            var data = ConfigurationManager.AppSettings[locationKey];
+            if (data == null)
+                return null;
+            Location location = JsonConvert.DeserializeObject<Location>(data);
+            return location;
+        }
+    }
+}
