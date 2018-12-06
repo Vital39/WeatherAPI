a6692758dd6337aa94134427be1673502f03c233
Андрей Демидюк <andrew.demidyuk@outlook.com>
2018-12-06 09:59:04 +0200

    Add default time zone to Unix formatter

--- Services/UnixFormatter.cs
+++ Services/UnixFormatter.cs
@@ -11,25 +11,25 @@ namespace WeatherApp.Services
     {  
         public string GetShortDate(long unixTime, string timeZoneId)
         {
-            var dateTime = GetDateTime(unixTime, timeZoneId);
+            var dateTime = GetDateTime(unixTime, timeZoneId=null);
             string mounth = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(dateTime.Month);           
             return $"{dateTime.Day} {mounth.Substring(0, 3)} {dateTime.Year}";
         }
         public string GetTime(long unixTime, string timeZoneId)
         {
-            var dateTime = GetDateTime(unixTime, timeZoneId);
+            var dateTime = GetDateTime(unixTime, timeZoneId=null);
             return $"{dateTime.Hour}:{dateTime.Minute}";
         }
         public string GetWeekDay(long unixTime, string timeZoneId)
         {
-            var dateTime = GetDateTime(unixTime, timeZoneId);
+            var dateTime = GetDateTime(unixTime, timeZoneId=null);
             var weekDay = CultureInfo.CurrentCulture.DateTimeFormat.GetDayName(dateTime.DayOfWeek);
             return $"{weekDay}";
         }
 
         public string GetNumberAndMonth(long unixTime, string timeZoneId)
         {
-            var dateTime = GetDateTime(unixTime, timeZoneId);
+            var dateTime = GetDateTime(unixTime, timeZoneId=null);
             string mounth = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(dateTime.Month);
             return $"{dateTime.Day} {mounth}";
         }
@@ -39,6 +39,8 @@ namespace WeatherApp.Services
             var dateTimeOff = DateTimeOffset.FromUnixTimeSeconds(unixTime);
             try
             {
+                if (String.IsNullOrEmpty(timeZoneId))
+                    throw new TimeZoneNotFoundException();
                 var timeZone=TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
                 DateTime dateTime= TimeZoneInfo.ConvertTime(dateTimeOff, timeZone).UtcDateTime;
                 return dateTime;
